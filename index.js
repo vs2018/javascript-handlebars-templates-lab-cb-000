12c234305acc46bd67dc84b5e3e04bfab7518659
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 05:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("partial-template").innerHTML)
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
