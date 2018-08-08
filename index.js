07b6c715140d2f778e084af5b8c5a582f2a5d475
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 06:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ function createRecipe() {
   document.getElementById("main").innerHTML = template(recipe)
 }
 
+function updateRecipe() {
+  var recipe = getRecipeVals()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
 
 function getRecipeVals() {
   var ingredientsNodes = document.getElementsByName("ingredients")
