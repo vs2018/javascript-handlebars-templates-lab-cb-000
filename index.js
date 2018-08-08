739e4f4cdb2c800e59e2ff6c05aee281801d9f67
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 05:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 
   Handlebars.registerHelper('displayIngredient', function() {
-  if(this.state === "closed") {
+  
     return new Handlebars.SafeString(this.body)
-  } else {
-    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
-  }
+
 })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
