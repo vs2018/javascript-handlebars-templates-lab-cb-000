7f7c3e05f3347b7db7368b6993fbeac4deb206dc
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 05:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
