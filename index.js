307409d41dbaae3447604b78feffef74815c016a
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 06:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,14 +61,8 @@ function handlebarsSetup() {
 
 
 function init() {
-  //put any handlebars registrations here.
- Handlebars.registerHelper('displayIngredient', function(ingredient) {
-   return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
- })
- Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
- Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-   initForm()
-
+  handlebarsSetup()
+ initForm()
 
 
 
