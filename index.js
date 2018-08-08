a8b85adadf08973795b07f5e3cb3c7edfe8c60f1
Vishal Sharma <vsh1819@gmail.com>
2018-08-08 06:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+Handlebars.registerHelper('displayIngredient', function() {
+
+  return new Handlebars.SafeString(this.body)
+
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-
-  Handlebars.registerHelper('displayIngredient', function() {
-
-    return new Handlebars.SafeString(this.body)
+  
 
 })
 
